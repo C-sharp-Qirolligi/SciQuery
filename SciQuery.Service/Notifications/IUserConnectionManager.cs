90c88663cd5ed37f975945b38031bb056d8b8500
FirdavsAX <137472686+FirdavsAX@users.noreply.github.com>
2024-08-14 18:47:28 +0500

    asdas

--- SciQuery.Service/Notifications/IUserConnectionManager.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SciQuery.Service.Notifications;
-
-public interface IUserConnectionManager
-{
-    void KeepUserConnection(string userId, string connectionId);
-    void RemoveUserConnection(string connectionId);
-    List<string> GetUserConnections(string userId);
-}
--- SciQuery.Service/Notifications/Models/NotificationModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SciQuery.Service.Notifications.Models;
-
-public class NotificationModel
-{
-    public string NotificationTitle { get; set; }
-    public string NotificationBody { get; set; }
-    public string UserId { get; set; }
-}
-
